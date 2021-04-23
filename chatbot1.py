e393e71a3f577a07a99c27517b994329592b3d63
Wt666 <20431546@life.hkbu.edu.hk>
2021-04-23 23:34:15 +0800

    11th

--- chatbot1.py
+++ chatbot1.py
@@ -2,7 +2,7 @@
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 # The messageHandler is used for all message updates
-import configparser
+#import configparser
 import logging
 import redis
 
@@ -10,8 +10,8 @@ global redis1
 
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
+    #config = configparser.ConfigParser()
+    #config.read('config.ini')
     updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
     # You can set this logging module, so you will know when and why things do not work as expected
